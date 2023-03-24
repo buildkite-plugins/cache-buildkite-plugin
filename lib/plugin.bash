b598b3408e825eb38717ef7c3ed72147da5e4faa
Matías A. Bellone <matias.bellone@buildkite.com>
2023-03-24 04:11:40 -0300

    split lib/shared.bash into shared, plugin-specific and fs-backend files

---
+++ lib/plugin.bash
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -euo pipefail
+
+PLUGIN_PREFIX="CACHE"
+
+# Reads either a value or a list from the given env prefix
+function prefix_read_list() {
+  local prefix="$1"
+  local parameter="${prefix}_0"
+
+  if [ -n "${!parameter:-}" ]; then
+    local i=0
+    local parameter="${prefix}_${i}"
+    while [ -n "${!parameter:-}" ]; do
+      echo "${!parameter}"
+      i=$((i+1))
+      parameter="${prefix}_${i}"
+    done
+  elif [ -n "${!prefix:-}" ]; then
+    echo "${!prefix}"
+  fi
+}
+
+# Reads either a value or a list from plugin config
+function plugin_read_list() {
+  prefix_read_list "BUILDKITE_PLUGIN_${PLUGIN_PREFIX}_${1}"
+}
+
+
+# Reads either a value or a list from plugin config into a global result array
+# Returns success if values were read
+function prefix_read_list_into_result() {
+  local prefix="$1"
+  local parameter="${prefix}_0"
+  result=()
+
+  if [ -n "${!parameter:-}" ]; then
+    local i=0
+    local parameter="${prefix}_${i}"
+    while [ -n "${!parameter:-}" ]; do
+      result+=("${!parameter}")
+      i=$((i+1))
+      parameter="${prefix}_${i}"
+    done
+  elif [ -n "${!prefix:-}" ]; then
+    result+=("${!prefix}")
+  fi
+
+  [ ${#result[@]} -gt 0 ] || return 1
+}
+
+# Reads either a value or a list from plugin config
+function plugin_read_list_into_result() {
+  prefix_read_list_into_result "BUILDKITE_PLUGIN_${PLUGIN_PREFIX}_${1}"
+}
+
+# Reads a single value
+function plugin_read_config() {
+  local var="BUILDKITE_PLUGIN_${PLUGIN_PREFIX}_${1}"
+  local default="${2:-}"
+  echo "${!var:-$default}"
+}
--- lib/shared.bash
+++ lib/shared.bash
@@ -4,33 +4,6 @@ is_debug() {
   [[ "${BUILDKITE_PLUGIN_CACHE_DEBUG:-false}" =~ ^(true|on|1)$ ]]
 }
 
-# Reads either a value or a list from environment
-function prefix_read_list() {
-  local prefix="$1"
-  local parameter="${prefix}_0"
-
-  if [[ -n "${!parameter:-}" ]]; then
-    local i=0
-    local parameter="${prefix}_${i}"
-    while [[ -n "${!parameter:-}" ]]; do
-      echo "${!parameter}"
-      i=$((i+1))
-      parameter="${prefix}_${i}"
-    done
-  elif [[ -n "${!prefix:-}" ]]; then
-    echo "${!prefix}"
-  fi
-}
-
-# Returns a list of env vars in the form of BUILDKITE_PLUGIN_CACHE_PATHS_N
-list_cache_entries() {
-  while IFS='=' read -r name _ ; do
-    if [[ $name =~ ^(BUILDKITE_PLUGIN_CACHE_PATHS_[0-9]+) ]] ; then
-      echo "${BASH_REMATCH[1]}"
-    fi
-  done < <(env | sort) | uniq
-}
-
 # Hashes files and directories recursively
 hash_files() {
   local shasum="false"
@@ -58,19 +31,4 @@ hash_files() {
 build_manifest_cache_path()  {
   local manifest="$1"
   echo "${BUILDKITE_AGENT_CACHE_PATH?}/manifest/$(hash_files "$manifest")"
-}
-
-restore_cache() {
-  local from="$1"
-  local to="$2"
-  cp -av "$from" "$to"
-  ls -al "$to"
-}
-
-save_cache() {
-  local from="$1"
-  local to="$2"
-  mkdir -p "$to"
-  cp -av "$from" "$to"
-  ls -al "$to"
 }
