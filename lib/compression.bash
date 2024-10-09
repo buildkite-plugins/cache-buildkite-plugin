a69b8e3f18d43ebdb4d77b34db8305d2386f99bf
Peter Svensson <peter@sparetimecoders.com>
2024-10-09 16:52:32 +0200

    chore: support absolute caching path

--- lib/compression.bash
+++ lib/compression.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck source=lib/plugin.bash
 . "${DIR}/plugin.bash"
@@ -35,17 +35,32 @@ compress() {
   echo "Compressing ${COMPRESSED_FILE} with ${COMPRESSION}..."
 
   if [ "${COMPRESSION}" = 'tgz' ]; then
-    tar czf "${FILE}" "${COMPRESSED_FILE}"
+    if is_absolute_path "${COMPRESSED_FILE}"; then
+      tar czPf "${FILE}" "${COMPRESSED_FILE}"
+    else
+      tar czf "${FILE}" "${COMPRESSED_FILE}"
+    fi
   elif [ "${COMPRESSION}" = 'zip' ]; then
-    # because ZIP complains if the file does not end with .zip
-    zip -r "${FILE}.zip" "${COMPRESSED_FILE}"
-    mv "${FILE}.zip" "${FILE}"
+    if is_absolute_path "${COMPRESSED_FILE}"; then
+      local COMPRESS_DIR
+      COMPRESS_DIR="$(dirname "${COMPRESSED_FILE}")"
+      echo "Shifting to absolute path ${COMPRESS_DIR}"
+      pushd "${COMPRESS_DIR}" || exit 1
+      # because ZIP complains if the file does not end with .zip
+      zip -r "${FILE}.zip" "${COMPRESSED_FILE}"
+      popd || exit 1
+      mv "${COMPRESS_DIR}/${FILE}.zip" "${FILE}"
+    else
+      # because ZIP complains if the file does not end with .zip
+      zip -r "${FILE}.zip" "${COMPRESSED_FILE}"
+      mv "${FILE}.zip" "${FILE}"
+    fi
   fi
 }
 
 uncompress() {
   local FILE="$1"
-  local _RESTORE_PATH="$2" # pretty sure this is not necessary
+  local RESTORE_PATH="$2"
 
   local COMPRESSION=''
   COMPRESSION="$(plugin_read_config COMPRESSION 'none')"
@@ -53,10 +68,30 @@ uncompress() {
   echo "Cache is compressed, uncompressing with ${COMPRESSION}..."
 
   if [ "${COMPRESSION}" = 'tgz' ]; then
-    tar xzf "${FILE}"
+    if is_absolute_path "${RESTORE_PATH}"; then
+      tar xzPf "${FILE}"
+    else
+      tar xzf "${FILE}"
+    fi
   elif [ "${COMPRESSION}" = 'zip' ]; then
-    # because ZIP complains if the file does not end with .zip
-    mv "${FILE}" "${FILE}.zip"
-    unzip -o "${FILE}.zip"
+    if is_absolute_path "${RESTORE_PATH}"; then
+      local RESTORE_DIR
+      RESTORE_DIR="$(dirname "${RESTORE_PATH}")"
+      echo "Shifting to absolute path ${RESTORE_DIR}"
+      mkdir -p "${RESTORE_DIR}"
+      mv "${FILE}" "${RESTORE_DIR}/${FILE}.zip"
+      pushd "${RESTORE_DIR}" || exit 1
+      unzip -o "${FILE}.zip"
+      popd || exit 1
+    else
+      # because ZIP complains if the file does not end with .zip
+      mv "${FILE}" "${FILE}.zip"
+      unzip -o "${FILE}.zip"
+    fi
   fi
+}
+
+is_absolute_path() {
+  local FILEPATH="${1}"
+  [ "${FILEPATH:0:1}" = "/" ]
 }
